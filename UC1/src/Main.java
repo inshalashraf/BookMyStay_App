f882116ee85ecd57de0d4c96fed73d00443cbb27
inshalashraf <mdinshalashraf@gmail.com>
2026-03-24 08:50:05 +0530

    UC1

---
+++ UC1/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("============================================================");
+        System.out.println("       Welcome to Book My Stay App");
+        System.out.println("       Application Name : Book My Stay");
+        System.out.println("       Version          : 1.0");
+        System.out.println("============================================================");
+        System.out.println("System has started successfully.");
+        System.out.println("============================================================");
+
+    }
+}
